f052801dd8131e04916498ca8f743af7ede4a1f4
Arun Punaroor <68643820+arunpunaroor@users.noreply.github.com>
2021-05-02 22:57:32 +0200

    adding url info for api/types

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/collectibles', include('retrocollector_api.urls')),
+    path('api/', include('retrocollector_api.urls')),
 ]
